5dff3b52bece341be54a4c19898eefcab2f734b0
gabefreedman <32077552+gabefreedman@users.noreply.github.com>
2019-04-11 11:50:47 -0400

    Add basic Index creation function
    Create Index class, can store list of companies in tick_items attribute. Each one points to specific Stock object. build_index function created to generate Index object.

--- portfolio.py
+++ portfolio.py
@@ -16,7 +16,43 @@ s = Stock(s_name)
 class Index:
     def __init__(self, name):
         self.name = name
+        self.tick_items = []
+    
+    def add_company(self, ticker):
+        s = Stock(ticker)
+        self.tick_items.append(s)
+    
+    def remove_company(self, ticker):
+        s = Stock(ticker)
+        self.tick_items.remove(s)
 
 class Portfolio:
     pass
 
+
+def build_index(name, companies=None):
+    ''' Create custom Index object to store stock information
+    
+    Parameters
+    ----------
+    name (str) : Name for Index object (eg. VICE, ABCD, SMMR)
+    companies (list, optional) : If provided, list of companies to add to index
+                                 Calls add_company class function
+    
+    Returns
+    -------
+    index (Index) : Index object containing list of companies as Stock instances
+    
+    '''
+    
+    index = Index(name)
+    
+    if companies:
+        # Force uppercase for ticker symbols
+        # Remove duplicate tickers
+        companies = [x.upper() for x in companies]
+        companies = list(set(companies))
+        for cmp in companies:
+            index.add_company(cmp)
+    
+    return index
