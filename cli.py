8cf4e462cdef73d13cce939f3635ff167d29827b
gabefreedman <32077552+gabefreedman@users.noreply.github.com>
2019-05-03 14:24:51 -0400

    Add help and exit commands to console input
    Will be moving underlying function and function dictionary to separate module.

--- cli.py
+++ cli.py
@@ -6,19 +6,24 @@ Created on Tue Apr 30 09:45:39 2019
 """
 
 import re
+import sys
 
-commands = ['build', 'exist']
-
-valid_answers = ['y', 'yes', 'n', 'no']
+help_func = {'build' : 'Build Index from input ticker symbols',
+            'exist' : 'Prints existing Index objects being tracked',
+            'table' : 'Pick existing Index and update table of financial metrics',
+            'exit' : 'Exit program', 
+            'help' : 'Display available commands'}
 
 def yes_no_prompt(prompt):
+    valid_answers = ['y', 'yes', 'n', 'no']
+    
     while True:
         try:
             answer = input(prompt)
         except ValueError:
             print('Invalid input type. Strings only, please.')
             continue
-        
+
         if answer.lower() not in valid_answers:
             print('Invalid answer, must be y/n response.\n')
             continue
@@ -28,16 +33,16 @@ def yes_no_prompt(prompt):
 
 def parse_tickers(symbol):
     pattern = r',\s+|\s+'
-    
+
     symbol = symbol.upper()
     symbol = re.split(pattern, symbol)
     symbol = list(filter(None, symbol))
-    
+
     return symbol
 
 def build_index():
     print('Index has been built. Yay.')
-    
+
 
 def _build():
     tick_items = []
@@ -53,7 +58,7 @@ def _build():
     tick_items.extend(tickers)
     print('Current tickers in {}: {}'.format(ind_name, tick_items))
     add_more = yes_no_prompt('Add more tickers to {}? '.format(ind_name))
-    
+
     while add_more in ['y', 'yes']:
         print('Enter ticker symbols to add to index')
         tickers = input('>>> ')
@@ -61,7 +66,7 @@ def _build():
         tick_items.extend(tickers)
         print('Current tickers in {}: {}'.format(ind_name, tick_items))
         add_more = yes_no_prompt('Add more tickers to {}? '.format(ind_name))
-    
+
     tick_items = list(set(tick_items))
     print('\nFinal index {}'.format(ind_name))
     print('Tickers: {}'.format(tick_items))
@@ -75,27 +80,49 @@ def _build():
             tick_items = [x for x in tick_items if x not in remove_tick]
             print('Tickers: {}'.format(tick_items))
             remove_more = yes_no_prompt('Remove more tickers? ')
-        
+
         print('\nFinal index {}'.format(ind_name))
         print('Tickers: {}'.format(tick_items))
         print('To remove tickers type remove. Otherwise hit ENTER to build Index.')
         remove_or_build = input('>>> ')
-    
+
     build_index()
     print('We\'re done here.')
+
+def _exist():
+    print('Nothing exists')
+
+def _table():
+    print('What table?')
+
+def _help():
+    print('--------AVAILABLE COMMANDS--------')
+    for key, val in help_func.items():
+        print('{} : {}\n'.format(key, val))
+
+def _exit():
+    sys.exit()
+
+commands = {'build' : _build,
+            'exist' : _exist,
+            'table' : _table,
+            'exit' : _exit, 
+            'help' : _help}
+
+def main():
+    print('\n---------STOCK INDEX CREATION AND TRACKING PROGRAM---------\n')
+    print('Type \'help\' to show available commands')
     
-
-print('---------STOCK INDEX CREATION AND TRACKING PROGRAM---------')
-print('\n\nAvailable commands:')
-print('\tbuild : Build Index from input ticker symbols')
-print('\texist : Prints existing Index objects being tracked')
-
-input1 = input('>>> ')
-while input1 not in commands:
-    print('Invalid command. Available commands are [' + ', '.join(commands) + ']')
     input1 = input('>>> ')
+    while input1 != 'exit':
+        while input1 not in commands.keys():
+            print('Invalid command. Type \'help\' for available commands')
+            input1 = input('>>> ')
 
-print('You entered: {}'.format(input1))
+        commands[input1]()
+        
+        input1 = input('>>> ')
 
-if input1 == 'build':
-    _build()
+if __name__ == '__main__':
+    main()
+
